020f94524074e748e114e07f79ad7b8389e78f00
yangxu <xu.yang@sipingsoft.com>
2020-09-18 12:45:56 +0800

    feat: 多线程顺序打印

--- src/com/bailiny/leetcode/editor/cn/Foo.java
+++ src/com/bailiny/leetcode/editor/cn/Foo.java
@@ -3,7 +3,77 @@ package com.bailiny.leetcode.editor.cn;
 /**
  * @author Yang Xu
  * @date 2020/9/18 11:07
- * @description:
+ * @description: 1114. °´Ðò´òÓ¡
  */
-public class Foo {
+class Foo {
+
+    private boolean firstFinished = false;
+    private boolean secondFinished = false;
+    private Object lock = new Object();
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (lock) {
+            printFirst.run();
+            firstFinished = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (lock) {
+            while (!firstFinished) {
+                lock.wait();
+            }
+
+            printSecond.run();
+            secondFinished = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (lock) {
+            while (!secondFinished) {
+                lock.wait();
+            }
+            printThird.run();
+        }
+    }
+}
+
+class FooTest {
+    public static void main(String[] args) throws InterruptedException {
+        Foo foo = new Foo();
+        Runnable a = () -> {
+            try {
+                foo.first(() -> System.out.print("first"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable b = () -> {
+            try {
+                foo.second(() -> System.out.print("second"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable c = () -> {
+            try {
+                foo.third(() -> System.out.print("third"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread t1 = new Thread(a);
+        Thread t2 = new Thread(b);
+        Thread t3 = new Thread(c);
+        t3.start();
+        t1.start();
+        t2.start();
+    }
 }
