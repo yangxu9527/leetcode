82946c60bd4a7d8a939ae685e657ef2d56ad97fd
yangxu <yangxu9527@foxmail.com>
2020-09-20 15:33:52 +0800

    feat: 使用reentrantLock解决

--- src/com/bailiny/leetcode/editor/cn/Foo.java
+++ src/com/bailiny/leetcode/editor/cn/Foo.java
@@ -1,5 +1,8 @@
 package com.bailiny.leetcode.editor.cn;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Yang Xu
  * @date 2020/9/18 11:07
@@ -45,9 +48,59 @@ class Foo {
     }
 }
 
+class Foo2 {
+
+    private boolean firstFinished = false;
+    private boolean secondFinished = false;
+    private ReentrantLock reentrantLock = new ReentrantLock();
+    private Condition condition1 = reentrantLock.newCondition();
+    private Condition condition2 = reentrantLock.newCondition();
+
+    public Foo2() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        reentrantLock.lock();
+        try {
+            printFirst.run();
+            firstFinished = true;
+            condition1.signal();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        reentrantLock.lock();
+        try {
+            while (!firstFinished) {
+                condition1.await();
+            }
+            printSecond.run();
+            secondFinished = true;
+            condition2.signal();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        reentrantLock.lock();
+        try {
+            while (!secondFinished) {
+                condition2.await();
+            }
+            printThird.run();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+}
+
 class FooTest {
     public static void main(String[] args) throws InterruptedException {
-        Foo foo = new Foo();
+        Foo2 foo = new Foo2();
         Runnable a = () -> {
             try {
                 foo.first(() -> System.out.print("first"));
