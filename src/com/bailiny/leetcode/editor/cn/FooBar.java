b7ec82d42d8b7df39f0894a85d911bc3ab2eff9c
yangxu <yangxu9527@foxmail.com>
2020-09-20 15:51:22 +0800

    feat: 交替打印

---
+++ src/com/bailiny/leetcode/editor/cn/FooBar.java
@@ -0,0 +1,40 @@
+package com.bailiny.leetcode.editor.cn;
+
+public class FooBar {
+
+    private int n;
+    // ∏√À≠¥Ú”°£¨0 foo, 1 bar
+    private int whoPrint;
+
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (whoPrint == 1) {
+                    this.wait();
+                }
+                printFoo.run();
+                whoPrint = 1;
+                this.notifyAll();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (whoPrint == 0) {
+                    this.wait();;
+                }
+                printBar.run();
+                whoPrint = 0;
+                this.notifyAll();
+            }
+        }
+    }
+}
